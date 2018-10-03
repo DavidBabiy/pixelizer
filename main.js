b21da6d09a2c78fbb8772d0f40261131f2ed0623
Taras Halynskyi <off.bk90@gmail.com>
2018-10-03 19:34:10 +0300

    add main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+class Human {
+    constructor() {
+    }
+
+    sayHello () {
+        console.log("Hello I am " + this.name);
+    }
+
+}
+
+let human1 = new Human();
+human1.name = "Taras";
+
+human1.sayHello();
+
+
+
+
+
+
