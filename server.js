c729c5aea8f06ac526bfba8d9100397753b7b842
DavidBabiy <david.babiy1995@gmail.com>
2018-10-03 18:35:17 +0300

    update main form

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
-for(let x=0; x<5; x++){
-    console.log("Hello world");
-}
+let express = require('express');
+let app = express();
+let path = require('path');
+const PORT = 3000;
 
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/hello', function (req, res) {
+    let name = req.query.name;
+    if(name == undefined){
+        res.send('Please provide name');
+    } else {
+        res.send('Hello ' + name);
+    }
+});
+
+
+
+app.listen(PORT, function(){
+    console.log('Server start at port ' + PORT);
+});
