793ae5fd6a8b39a493d9828f59805f068513c867
Taras Halynskyi <off.bk90@gmail.com>
2018-10-03 20:26:50 +0300

    fix main form

---
+++ public/index.js
@@ -0,0 +1,9 @@
+function submitForm() {
+    console.log("Hello I am MagicButton");
+    return false;
+}
+
+function  handleFiles(files) {
+    let selectedFile = files[0];
+    console.log(selectedFile);
+}
