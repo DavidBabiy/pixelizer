ac899493f959c0cf9aa7a1141b355905b20e4984
Taras Halynskyi <off.bk90@gmail.com>
2018-10-22 01:18:00 +0300

    fix

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.post('/file/upload', function (req, res) {
     console.log("Starting image processing");
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let image = req.files.image;
+    console.log(image.mimetype);
     // Use the mv() method to place the file somewhere on your server
     image.mv(FILES_UPLOAD_FOLDER + '/' + image.name, function (err) {
         if (err) return res.status(500).send(err);
@@ -39,21 +40,23 @@ app.post('/file/upload', function (req, res) {
             if (err) return res.status(500).send('Error while resizing image');
             img.scaleToFit(300, 300)
                 .quality(60)
-                .write(FILES_UPLOAD_FOLDER + '/' + image.name);
-            console.log("File resized");
-            getPixels(FILES_UPLOAD_FOLDER + '/' + image.name, function (err, pixels) {
-                if (err) return res.status(500).send('Error while reading image pixels');
-                // TODO: Here is data that you need
-                console.log(pixels.data);
-                console.log(pixels.shape);
-                let width = pixels.shape[0];
-                let height = pixels.shape[1];
-                console.log("File size => " + width + " x " + height);
-                res.sendFile(FILES_UPLOAD_FOLDER + '/' + image.name, {root: __dirname}, function (err) {
-                    if (err) return res.status(500).send(err);
-                    console.log('Image sent to client');
+                .write(FILES_UPLOAD_FOLDER + '/' + image.name, function () {
+                    console.log("File resized");
+                    getPixels(FILES_UPLOAD_FOLDER + '/' + image.name, image.mimetype, function (err, pixels) {
+                        console.log(err);
+                        if (err) return res.status(500).send('Error while reading image pixels');
+                        // TODO: Here is data that you need
+                        console.log(pixels.data);
+                        console.log(pixels.shape);
+                        let width = pixels.shape[0];
+                        let height = pixels.shape[1];
+                        console.log("File size => " + width + " x " + height);
+                        res.sendFile(FILES_UPLOAD_FOLDER + '/' + image.name, {root: __dirname}, function (err) {
+                            if (err) return res.status(500).send(err);
+                            console.log('Image sent to client');
+                        });
+                    })
                 });
-            })
 
         });
 
@@ -66,6 +69,7 @@ app.listen(PORT, function () {
     if (!fs.existsSync(FILES_UPLOAD_FOLDER)) {
         fs.mkdirSync(FILES_UPLOAD_FOLDER);
     }
-
 });
+
+
 
