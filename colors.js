c88b454847a4daa6bb1c657796e69362834ba647
Taras Halynskyi <off.bk90@gmail.com>
2018-10-24 20:08:08 +0300

    split modules, fix png convert

--- colors.js
+++ colors.js
@@ -1,18 +1,26 @@
-export const ansiColorsMap = {
-    97: 'BRIGHT_WHITE',
+const ansiColorsMap = {
+    // 97: 'BRIGHT_WHITE',
     96: 'BRIGHT_CYAN',
     95: 'BRIGHT_MAGENTA',
     94: 'BRIGHT_BLUE',
     93: 'BRIGHT_YELLOW',
     92: 'BRIGHT_GREEN',
     91: 'BRIGHT_RED',
-    90: 'BRIGHT_BLACK',
-    37: 'WHITE',
+    // 90: 'BRIGHT_BLACK',
+    // 37: 'WHITE',
     36: 'CYAN',
     35: 'MAGENTA',
     34: 'BLUE',
     33: 'YELLOW',
     32: 'GREEN',
     31: 'RED',
-    30: 'BLACK'
-}
+    // 30: 'BLACK',
+
+    // TODO: tmp fix, bugfix for Spring ansi colors
+    90: 'BRIGHT_WHITE',
+    30: 'BRIGHT_BLACK',
+    37: 'WHITE',
+    97: 'BLACK'
+};
+
+module.exports = { ansiColorsMap: ansiColorsMap };
---
+++ core.js
@@ -0,0 +1,85 @@
+const colors = require('./colors');
+const fs = require('fs');
+const getPixels = require("get-pixels");
+const colorConvert = require('color-convert');
+const jimp = require('jimp');
+const os = require('os');
+
+/** @type Core */
+let instance;
+
+/** @type Properties */
+const Properties = require('./properties').get();
+module.exports.Properties = Properties;
+
+function Core() {
+}
+
+Core.prototype.parsePixels = function (req, res, image) {
+     getPixels(Properties.FILES_UPLOAD_FOLDER + '/' + image.name, image.mimetype, (err, pixels) => {
+        if (err) return res.status(500).send('Error while reading image pixels');
+        let width = pixels.shape[0];
+        let height = pixels.shape[1];
+        let channels = pixels.shape[2];
+        let pixelsMat = [];
+        let columnCounter = 0;
+        let rowCounter = 0;
+        pixelsMat[0] = [];
+        let textFilename = image.name.split('.')[0] + ".txt";
+        fs.writeFile(Properties.FILES_UPLOAD_FOLDER + '/' + textFilename, '', (err) => {
+            if (err) throw err;
+        });
+        let writeStream = fs.createWriteStream(Properties.FILES_UPLOAD_FOLDER + '/' + textFilename);
+        for (let i = 0; i < pixels.data.length; i += channels) {
+            if (columnCounter === width) {
+                rowCounter++;
+                pixelsMat[rowCounter] = [];
+                columnCounter = 0;
+                writeStream.write('\r\n', 'utf-8');
+            }
+            let pixel = {};
+            pixel.r = pixels.data[i];
+            pixel.g = pixels.data[i + 1];
+            pixel.b = pixels.data[i + 2];
+            pixel.a = pixels.data[i + 3];
+            pixelsMat[rowCounter].push(pixel);
+            columnCounter++;
+            let ansiColorCode = colorConvert.rgb.ansi16(pixel.r, pixel.g, pixel.b);
+            writeStream.write('${AnsiColor.' + colors.ansiColorsMap[ansiColorCode] + '}', 'utf-8');
+            writeStream.write('█', 'utf-8');
+        }
+
+        writeStream.on("finish", () => {
+            res.sendFile(Properties.FILES_UPLOAD_FOLDER + '/' + textFilename, {root: __dirname}, (err) => {
+                if (err) return res.status(500).send(err);
+                console.log('Image sent to client');
+            });
+        });
+        writeStream.end();
+    })
+};
+
+Core.prototype.convertImage = function (req, res, image) {
+    image.mv(Properties.FILES_UPLOAD_FOLDER + '/' + image.name, (err) => {
+        if (err) return res.status(500).send(err);
+        jimp.read(Properties.FILES_UPLOAD_FOLDER + '/' + image.name, (err, img) => {
+            if (err) return res.status(500).send('Error while resizing image');
+            img.scaleToFit(Properties.MAX_FILE_RESOLUTION, Properties.MAX_FILE_RESOLUTION)
+                .quality(100)
+                .write(Properties.FILES_UPLOAD_FOLDER + '/' + image.name, () => {
+                    console.log("File resized");
+                    this.parsePixels(req, res, image)
+                });
+        });
+    });
+};
+
+/**
+ * @returns {Core}
+ */
+function get() {
+    instance = instance ? instance : new Core();
+    return instance;
+}
+
+module.exports.get = get;
