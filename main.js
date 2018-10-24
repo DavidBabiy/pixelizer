609c336afe21aaa09fc471e7d6338c44947f9185
DavidBabiy <43074459+DavidBabiy@users.noreply.github.com>
2018-10-24 18:09:01 +0300

    prepare array of pixels

--- main.js
+++ main.js
@@ -1,20 +1,81 @@
-/*class Human {
-    constructor() {
+function loopExample() {
+    let results = [];
+    let x = 2;
+    let y;
+    for (x = 2; x < 10; x += 2) {
+        y = 3 * x * x;
+        results.push(y)
     }
-
-    sayHello () {
-        console.log("Hello I am " + this.name);
-    }
-
+    return results
 }
 
-let human1 = new Human();
-human1.name = "Taras";
+function ifExample() {
+    let results = [];
+    let x = 2;
+    let y;
+    if (x == 2) {
+        y = 3 * x * x;
+        x += 2;
+        results.push(y);
+    }
+    if (x == 4) {
+        y = 3 * x * x;
+        x += 2;
+        results.push(y);
+    }
+    if (x == 6) {
+        y = 3 * x * x;
+        x += 2;
+        results.push(y);
+    }
+    if (x == 8) {
+        y = 3 * x * x;
+        results.push(y);
+    }
+    return results;
+}
 
-human1.sayHello();*/
+function singleIfExample() {
+    let results = [];
+    let x = 2;
+    let y;
+    if (x < 10) {
+        y = 3 * x * x;
+        results.push(y)
+    }
+    return results;
+}
 
+function singleIfRecursiveExample(x, arr) {
+    let y;
+    if (x < 10) {
+        y = 3 * x * x;
+        arr.push(y);
+        x += 2;
+        singleIfRecursiveExample(x, arr);
+    }
+    return arr
+}
 
+let correctResult = loopExample();
 
+if (JSON.stringify(correctResult) === JSON.stringify(ifExample())) {
+    console.log("ifExample function test passed")
+} else {
+    console.error("ifExample function test failed");
+    console.error("correctResult => " + correctResult + " | function returned => " + ifExample())
+}
 
+if (JSON.stringify(correctResult) === JSON.stringify(singleIfExample())) {
+    console.log("singleIfExample function test passed")
+} else {
+    console.error("singleIfExample function test failed");
+    console.error("correctResult => " + correctResult + " | function returned => " + singleIfExample())
+}
 
-
+if (JSON.stringify(correctResult) === JSON.stringify(singleIfRecursiveExample(2, []))) {
+    console.log("singleIfRecursiveExample function test passed")
+} else {
+    console.error("singleIfRecursiveExample function test failed");
+    console.error("correctResult => " + correctResult + " | function returned => " + singleIfRecursiveExample(2, []))
+}
--- server.js
+++ server.js
@@ -4,21 +4,23 @@ const path = require('path');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const getPixels = require("get-pixels");
-const ndarray = require("ndarray");
 const jimp = require('jimp');
+const convert = require('color-convert');
+const os = require('os');
 
 
-const PORT = 3200;
+
+const PORT = 3000;
 const FILES_UPLOAD_FOLDER = 'storage';
 
 app.use(fileUpload());
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/hello', function (req, res) {
+app.get('/hello',(req, res) => {
     let name = req.query.name;
     if (name === undefined) {
         res.send('Please provide name');
@@ -27,31 +29,58 @@ app.get('/hello', function (req, res) {
     }
 });
 
-app.post('/file/upload', function (req, res) {
+app.post('/file/upload',(req, res) => {
     if (!req.files) return res.status(400).send('No files were uploaded.');
     console.log("Starting image processing");
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let image = req.files.image;
     console.log(image.mimetype);
     // Use the mv() method to place the file somewhere on your server
-    image.mv(FILES_UPLOAD_FOLDER + '/' + image.name, function (err) {
+    image.mv(FILES_UPLOAD_FOLDER + '/' + image.name,(err) => {
         if (err) return res.status(500).send(err);
         jimp.read(FILES_UPLOAD_FOLDER + '/' + image.name, (err, img) => {
             if (err) return res.status(500).send('Error while resizing image');
-            img.scaleToFit(300, 300)
+            img.scaleToFit(30, 30)
                 .quality(60)
-                .write(FILES_UPLOAD_FOLDER + '/' + image.name, function () {
+                .write(FILES_UPLOAD_FOLDER + '/' + image.name,() => {
                     console.log("File resized");
-                    getPixels(FILES_UPLOAD_FOLDER + '/' + image.name, image.mimetype, function (err, pixels) {
-                        console.log(err);
+                    getPixels(FILES_UPLOAD_FOLDER + '/' + image.name, image.mimetype,(err, pixels) => {
                         if (err) return res.status(500).send('Error while reading image pixels');
                         // TODO: Here is data that you need
-                        console.log(pixels.data);
-                        console.log(pixels.shape);
                         let width = pixels.shape[0];
                         let height = pixels.shape[1];
-                        console.log("File size => " + width + " x " + height);
-                        res.sendFile(FILES_UPLOAD_FOLDER + '/' + image.name, {root: __dirname}, function (err) {
+                        let channels = pixels.shape[2];
+                        let pixelsMat = [];
+                        let columnCounter = 0;
+                        let rowCounter = 0;
+                        pixelsMat[0] = [];
+                        let textFilename = path.basename(FILES_UPLOAD_FOLDER + '/' + image.name, path.extname(FILES_UPLOAD_FOLDER + '/' + image.name));
+                        fs.writeFile(FILES_UPLOAD_FOLDER + '/' + textFilename + ".txt", '', (err) => {
+                            if(err) throw err;
+                        });
+                        let writeStream = fs.createWriteStream(FILES_UPLOAD_FOLDER + '/' + textFilename + ".txt");
+                            for(let i = 0; i < pixels.data.length; i+=channels) {
+                                if (columnCounter === width) {
+                                    rowCounter++;
+                                    pixelsMat[rowCounter] = [];
+                                    columnCounter = 0;
+                                    writeStream.write('\n', 'utf-8');
+                                }
+                                let pixel = {};
+                                pixel.r = pixels.data[i];
+                                pixel.g = pixels.data[i + 1];
+                                pixel.b = pixels.data[i + 2];
+                                pixel.a = pixels.data[i + 3];
+                                //pixelsMat.push(pixel);
+                                pixelsMat[rowCounter].push(pixel);
+                                columnCounter++;
+                                writeStream.write('█', 'utf-8');
+                            }
+                            writeStream.end();
+                            console.log('First dimension length(height) = ' + pixelsMat.length);
+                            console.log('Second dimension length(width) = ' + pixelsMat[0].length);
+                            console.log(pixelsMat[0]);
+                            res.sendFile(FILES_UPLOAD_FOLDER + '/' + textFilename + ".txt", {root: __dirname},(err) => {
                             if (err) return res.status(500).send(err);
                             console.log('Image sent to client');
                         });
@@ -64,7 +93,7 @@ app.post('/file/upload', function (req, res) {
 
 });
 
-app.listen(PORT, function () {
+app.listen(PORT,() => {
     console.log('Server start at port ' + PORT);
     if (!fs.existsSync(FILES_UPLOAD_FOLDER)) {
         fs.mkdirSync(FILES_UPLOAD_FOLDER);
