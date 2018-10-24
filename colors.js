4e6cc170cddacdf783214e7a62b2ff3e30f73e58
DavidBabiy <david.babiy1995@gmail.com>
2018-10-24 18:04:14 +0300

    prepare array of pixels

---
+++ colors.js
@@ -0,0 +1,18 @@
+export const ansiColorsMap = {
+    97: 'BRIGHT_WHITE',
+    96: 'BRIGHT_CYAN',
+    95: 'BRIGHT_MAGENTA',
+    94: 'BRIGHT_BLUE',
+    93: 'BRIGHT_YELLOW',
+    92: 'BRIGHT_GREEN',
+    91: 'BRIGHT_RED',
+    90: 'BRIGHT_BLACK',
+    37: 'WHITE',
+    36: 'CYAN',
+    35: 'MAGENTA',
+    34: 'BLUE',
+    33: 'YELLOW',
+    32: 'GREEN',
+    31: 'RED',
+    30: 'BLACK'
+}
