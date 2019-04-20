3753170cf5ff2830fbaf100daf19029dff0af204
DavidBabiy <david.babiy1995@gmail.com>
2019-04-21 01:17:10 +0300

    changed resolution deleted lab2

--- typescript/src/Properties.ts
+++ typescript/src/Properties.ts
@@ -2,4 +2,4 @@ import * as path from 'path';
 
 export const PORT = 3200;
 export const FILES_UPLOAD_FOLDER = path.join(__dirname, '../..', '/storage');
-export const MAX_FILE_RESOLUTION = 300;
+export const MAX_FILE_RESOLUTION = 50;
